4b710a5f00cbf6f7eb857c226376df5d0e709e2f
Keem <mr.sahapap@gmail.com>
2023-08-04 05:23:20 +0700

    feat: add and style <footer>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,22 @@ function Footer() {
       <div className="footer-triangle-container">
         <div className="footer-triangle" />
       </div>
-      <div className="footer"></div>
+      <div className="footer">
+        <div className="footer-wrapper">
+          <div className="contact-me">
+            Contact <span className="contact-me-me">Me</span>
+          </div>
+          <a href="mailto: mr.sahapap.sk@gmail.com" className="email">
+            mr.sahapap.sk@gmail.com
+          </a>
+          <div className="icon-wrapper">
+            <img src="src\images\facebook.png" />
+            <img src="src\images\instagram.png" />
+            <img src="src\images\linkedin.png" />
+            <img src="src\images\twitter.png" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
